3f55df83436f6019fa113ba45b5fb264e33f587f
OswaldoBifarini <jgbifarini@gmail.com>
2024-12-05 13:37:12 -0500

    Creacion de repositorio

---
+++ src/main/java/ec/edu/espoc/josueguerrero2025/JosueGuerrero2025.java
@@ -0,0 +1,8 @@
+package ec.edu.espoc.josueguerrero2025;
+
+public class JosueGuerrero2025 {
+
+    public static void main(String[] args) {
+        System.out.println("Sistema de Gestion Pedidos Cafeteria");
+    }
+}
